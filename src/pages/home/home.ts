19b35f9c27dbae5fd0ff41c9a8c13e039356b9f5
unknown <sivaprakash003@gmail.com>
2018-08-11 19:32:47 +0530

    uichange

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Nav } from 'ionic-angular';
 import {LandingpagePage} from '../landingpage/landingpage';
+import {ProfilePage}from '../profile/profile'
 
 @Component({
   selector: 'page-home',
@@ -9,13 +10,17 @@ import {LandingpagePage} from '../landingpage/landingpage';
 })
 export class HomePage {
   public rootPage:any
-  // @ViewChild(Nav) nav: Nav;
-  constructor(public navCtrl: NavController,public nav:Nav) { }
+  @ViewChild(Nav) nav: Nav;
+  constructor(public navCtrl: NavController) { }
 
   openPage(page){
   switch(page){
   case 'dashboard':
   this.nav.setRoot(LandingpagePage);
+  break;
+
+  case 'profile':
+  this.nav.setRoot(ProfilePage);
   break;
   default:
   this.nav.setRoot(LandingpagePage);
